4cd4354b58d21ff4dd4b3f2d345e38c5dcb2d403
flynnwt <flynnwt@gmail.com>
2018-12-08 17:05:13 -0600

    stuff working

--- README.md
+++ README.md
@@ -2,18 +2,52 @@
 Javascript for Elastos Carrier 
 
 <pre>
-<b>$ jailastos</b>
-Starting...
-Registering jai interfaces.
+<b>wtf@ubuntu:~/shares/Shared/projects/jailastos/src/jailastos$ jailastos</b>
+Registering jailastos interfaces.
 Initializing...
 Executing jailastos.js...
-logLevel: 5
-2018-12-07 22:57:08 - DEBUG   : Try to loading persistence data from: ./data/carrier.data.
-2018-12-07 22:57:08 - DEBUG   : <new_networking_ex>:Bound successfully to 0.0.0.0:33446
-
-2018-12-07 22:57:08 - INFO    : Carrier: Carrier node created.
-address: ShsVZkfepc8UaDiis5SiRH5SRcwYk8qs132hyg7rS2enffvwZiwi
- nodeid: ChJcAmmtTQkxRXkYoNNIer8h34rBC2stvgxH248f9oWK
- userid: ChJcAmmtTQkxRXkYoNNIer8h34rBC2stvgxH248f9oWK
+2018-12-08 16:29:35 - INFO    : Carrier: Carrier node created.
+address: Sh00000000000ZNUpLwRCFDiis5SiRH5S
+ nodeid: Ch111111111111111111111111pDZDrBC2stvgxH248f9oWK
+ userid: Ch111111111111111111111111pDZDrBC2stvgxH248f9oWK
 error=0
+
+          User Info
+            name: wtf-jailastos (Ch111111111111111111111111pDZDrBC2stvgxH248f9oWK)
+           phone:   email:   region: US
+          gender: Texas Hippie Coalition  desc: Mouth of Cemetery Gates
+
+--- friend list ---
+felicity-windoze(E433333333333333333333333333333FZTT9pCiL1GuNtGkPAFFPk6TEoR7) status=disconnected presence=none
+Connection ready.
+Connection status: connected
+friend connected: E433333333333333333333333333333FZTT9pCiL1GuNtGkPAFFPk6TEoR7 connected
+<b>> friends</b>
+--- friends -------
+felicity-windoze(E433333333333333333333333333333FZTT9pCiL1GuNtGkPAFFPk6TEoR7) status=connected presence=none
+friend presence: E433333333333333333333333333333FZTT9pCiL1GuNtGkPAFFPk6TEoR7 busy
+<b>> add 3Wkk55555555555555555555uQeFXr3YfEEQkrAnpFrt8wMBU,for those about to rock</b>
+> friend added!
+          Friend Info
+          User Info
+            name:  (2944444444444444443444444GL)
+           phone:   email:   region: 
+          gender:   desc:
+          disconnected  none  label: 
+friend connected: 2944444444444444443444444GL connected
+<b>> msg E433333333333333333333333333333FZTT9pCiL1GuNtGkPAFFPk6TEoR7,i was so much older then</b>
+msg: (E433333333333333333333333333333FZTT9pCiL1GuNtGkPAFFPk6TEoR7) very well by reputation
+friend updated
+          Friend Info
+          User Info
+            name: wtf-chai (2944444444444444443444444GL)
+           phone:   email:   region: USA
+          gender: metal  desc:chailastos testing
+          connected  none  label: 
+<b>> friends</b>
+--- friends -------
+felicity-windoze(E433333333333333333333333333333FZTT9pCiL1GuNtGkPAFFPk6TEoR7) status=connected presence=none
+--- friends -------
+wtf-chai(2944444444444444443444444GL) status=connected presence=none
+
 </pre>
--- src/scripts/jailastos.js
+++ src/scripts/jailastos.js
@@ -1,16 +1,29 @@
-'use strict';
+//'use strict';
+// not with callbacks!!! must act like diff file
 
-var common = new Common();
+// nested objects don't appear to work; maybe could do as a func returning object
+
+// ----------------------------------------------------------------------------
+
+var common = new Common(); // print, settimeout, etc.
 
 function print(s) {
-   common.print('' + s);
+   if (typeof(s) == 'undefined') {
+      common.print('');
+   } else {
+      common.print('' + s);
+   }
 }
+var console = {};
+console.log = print;
+
+// ----------------------------------------------------------------------------
 
 var config = new ShellConfig();
 
 config.udpEnabled = true;
 config.dataDir = './data';
-config.logLevel = 5;
+config.logLevel = 4;
 config.logFile = 'jailastos.log';
 
 config.addNode('13.58.208.50', '33445', '89vny8MrKdDKs7Uta9RdVmspPjnRMdwMmaiEW27pZ7gh');
@@ -19,7 +32,173 @@ config.addNode('18.216.6.197', '33445', 'H8sqhRrQuJZ6iLtP2wanxt4LzdNrN2NNFnpPdq1
 config.addNode('52.83.171.135', '33445', '5tuHgK1Q4CYf4K5PutsEPK5E3Z7cbtEBdx7LwmdzqXHL');
 config.addNode('52.83.191.228', '33445', '3khtxZo89SBScAMaHhTvD68pPHiKxgZT6hTCSZZVgNEm');
 
-print('logLevel: ' + config.logLevel);
+// ----------------------------------------------------------------------------
+
+var cbIdle = function() {};
+callbacks.set('idle', 'cbIdle');
+
+var cbReady = function() {
+   print('Connection ready.');
+};
+callbacks.set('ready', 'cbReady');
+
+var cbConnection = function(status) {
+   print('Connection status: ' + status);
+};
+callbacks.set('connection', 'cbConnection');
+
+var cbFriendList = function(res) {
+   if (!res.empty) {
+      print('--- friend list ---');
+      print(res.name + '(' + res.userid + ') status=' + res.status + ' presence=' + res.presence);
+   }
+};
+callbacks.set('friendlist', 'cbFriendList');
+
+var cbFriendsIterate = function(res) {
+   if (!res.empty) {
+      print('--- friends -------');
+      print(res.name + '(' + res.userid + ') status=' + res.status + ' presence=' + res.presence);
+   }
+};
+callbacks.set('friendsiterate', 'cbFriendsIterate');
+
+var cbFriendConnection = function(userid, status) {
+   print('friend connected: ' + userid + ' ' + status);
+};
+callbacks.set('friendconnection', 'cbFriendConnection');
+
+var cbFriendPresence = function(userid, status) {
+   print('friend presence: ' + userid + ' ' + status);
+};
+callbacks.set('friendpresence', 'cbFriendPresence');
+
+var cbFriendAdded = function(f) {
+   print('friend added!');
+   printFriend(f);
+};
+callbacks.set('friendadded', 'cbFriendAdded');
+
+var cbFriendRequest = function(f, userid, msg) {
+   print('friend request! (' + userid + ') ' + msg);
+   printFriend(f);
+};
+callbacks.set('friendrequest', 'cbFriendRequest');
+
+var cbFriendInfo = function(f) {
+   print('friend updated');
+   printFriend(f);
+};
+callbacks.set('friendinfo', 'cbFriendInfo');
+
+var cbFriendMsg = function(userid, msg) {
+   print('msg: (' + userid + ') ' + msg);
+};
+callbacks.set('friendmessage', 'cbFriendMsg');
+
+// ----------------------------------------------------------------------------
+
+var checkError = function() {
+   rc = ela.getError();
+   if (rc) {
+      print('*** Error: ' + rc + ' ***');
+   }
+}
+
+var pad = '          ';
+
+var printUser = function(u) {
+   if (!u.empty) {
+      print(pad + 'User Info');
+      print(pad + '  name: ' + u.name + ' (' + u.userid + ')');
+      print(pad + ' phone: ' + u.phone + '  email: ' + u.email + '  region: ' + u.region);
+      print(pad + 'gender: ' + u.gender + '  desc: ' + u.description);
+   } else {
+      print('empty user!');
+   }
+};
+
+var printFriend = function(f) {
+   if (!f.empty) {
+      print(pad + 'Friend Info');
+      printUser(f);
+      print(pad + f.status + '  ' + f.presence + '  label: ' + f.label);
+   } else {
+      print('empty user!');
+   }
+};
+
+// ----------------------------------------------------------------------------
+
+var state = {
+
+};
+
+// ----------------------------------------------------------------------------
+// do something
+
+var prompt = new Prompt()
+var cbPrompt = function(cmdline) {
+   cmdline = cmdline.trim();
+   if (cmdline === '') {
+      return;
+   }
+   tokens = cmdline.split(' ');
+   cmd = tokens[0];
+   if (tokens.length > 1) {
+      tokens = tokens.slice(1).join(' ').split(',');
+   } else {
+      tokens = [];
+   }
+
+   if (cmdline === 'quit') {
+      ela.quit();
+   } else if (cmd === 'add') {
+      ela.addFriend(tokens[0], tokens[1])
+   } else if (cmd === 'geterror') {
+      print('Error: ' + ela.getError());
+   } else if (cmd === 'msg') {
+      ela.msgFriend(tokens[0], tokens[1]);
+   } else if (cmd === 'friends') {
+      ela.getFriends();
+   } else if (cmd === 'userinfo') {
+      self = ela.getSelf();
+      if (tokens.length == 0) {
+         printUser(self);
+      } else {
+         if (tokens.length < 2) {
+            print('Need parm,value');
+         } else {
+            if (tokens[0] === 'name') {
+               self.name = tokens[1];
+            } else if (tokens[0] === 'gender') {
+               self.gender = tokens[1];
+            } else if (tokens[0] === 'region') {
+               self.region = tokens[1];
+            } else if (tokens[0] === 'email') {
+               self.email = tokens[1];
+            } else if (tokens[0] === 'phone') {
+               self.phone = tokens[1];
+            } else if (tokens[0] === 'desc') {
+               self.description = tokens[1];
+            } else {
+               print('Need [name/gender/region/email/phone/desc]');
+            }
+            printUser(self);
+            ela.setSelf(self);
+            checkError();
+         }
+      }
+   }
+
+};
+callbacks.set('prompt', 'cbPrompt');
+
+//print('Starting prompt in 10 seconds...');
+//setTimeout(function() {
+//   prompt.readlineAsync(); // could have it end after n
+//}, 10000);
+prompt.readlineAsync();
 
 var ela = new Ela();
 var rc = ela.init(config);
@@ -32,6 +211,11 @@ print('address: ' + ela.getAddress());
 print(' nodeid: ' + ela.getNodeid());
 print(' userid: ' + ela.getUserid());
 print('error=' + ela.getError());
+print();
+var me = ela.getSelf();
+printUser(me);
+print();
+
 ela.run();
 
 while (true) {}
